5b9460756ef207dff38af4d96ba8f614610660bf
Markus <m.walther97@gmail.com>
2018-04-22 12:59:53 +0200

    Word (godoc)

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Package demoinfocs provides demo parser for the game Counter-Strike: Global Offensive.
+// Package demoinfocs provides a demo parser for the game Counter-Strike: Global Offensive.
 // It is based on the official demoinfogo tool by Valve as well as Stats Helix's demoinfo.
 // A good entry point to using the library is the Parser type.
 // Demo events are documented in the events package.
