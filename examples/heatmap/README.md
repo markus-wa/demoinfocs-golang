3905881d5671aaaae85e3d2d298d6fd3d689e3d4
Markus Walther <m.walther97@gmail.com>
2018-10-31 16:03:34 +0100

    examples: Cleaned up heatmap example

--- examples/heatmap/README.md
+++ examples/heatmap/README.md
@@ -2,10 +2,12 @@
 
 This example shows how to create a heatmap from positions where players fired their weapons from.
 
+See `heatmap.go` for the source code.
+
 ## Running the example
 
 `go run heatmap.go -demo /path/to/demo > out.jpg`
 
-This will create a JPEG of a radar overview with dots on all the locations where shots were fired.
+This will create a JPEG of a radar overview with dots on all the locations where shots were fired from.
 
 ![Resulting heatmap](https://raw.githubusercontent.com/markus-wa/demoinfocs-golang/master/examples/heatmap/heatmap.jpg)
--- examples/heatmap/heatmap.go
+++ examples/heatmap/heatmap.go
@@ -9,6 +9,7 @@ import (
 
 	heatmap "github.com/dustin/go-heatmap"
 	schemes "github.com/dustin/go-heatmap/schemes"
+	r2 "github.com/golang/geo/r2"
 
 	dem "github.com/markus-wa/demoinfocs-golang"
 	events "github.com/markus-wa/demoinfocs-golang/events"
@@ -16,8 +17,18 @@ import (
 	metadata "github.com/markus-wa/demoinfocs-golang/metadata"
 )
 
+const (
+	dotSize     = 15
+	opacity     = 128
+	jpegQuality = 90
+)
+
 // Run like this: go run heatmap.go -demo /path/to/demo.dem > out.jpg
 func main() {
+	//
+	// Parsing
+	//
+
 	f, err := os.Open(ex.DemoPathFromArgs())
 	checkError(err)
 	defer f.Close()
@@ -28,69 +39,61 @@ func main() {
 	header, err := p.ParseHeader()
 	checkError(err)
 
-	// Get metadata for the map that's being played
-	m := metadata.MapNameToMap[header.MapName]
+	// Get metadata for the map that the game was played on for coordinate translations
+	mapMetadata := metadata.MapNameToMap[header.MapName]
 
 	// Register handler for WeaponFire, triggered every time a shot is fired
-	points := []heatmap.DataPoint{}
-	var bounds image.Rectangle
+	var points []r2.Point
 	p.RegisterEventHandler(func(e events.WeaponFire) {
-		// Translate positions from in-game coordinates to radar overview
-		x, y := m.TranslateScale(e.Shooter.Position.X, e.Shooter.Position.Y)
+		// Translate positions from in-game coordinates to radar overview image pixels
+		x, y := mapMetadata.TranslateScale(e.Shooter.Position.X, e.Shooter.Position.Y)
 
-		// Track bounds to get around the normalization done by the heatmap library
-		bounds = updatedBounds(bounds, int(x), int(y))
-
-		// Add shooter's position as datapoint
-		// Invert Y since it expects data to be ordered from bottom to top
-		points = append(points, heatmap.P(x, y*-1))
+		points = append(points, r2.Point{X: x, Y: y})
 	})
 
-	// Parse to end
+	// Parse the whole demo
 	err = p.ParseToEnd()
 	checkError(err)
 
-	// Get map overview as base image
+	//
+	// Preparation of heatmap data
+	//
+
+	// Find bounding rectangle for points to get around the normalization done by the heatmap library
+	r2Bounds := r2.RectFromPoints(points...)
+	bounds := image.Rectangle{
+		Min: image.Point{X: int(r2Bounds.X.Lo), Y: int(r2Bounds.Y.Lo)},
+		Max: image.Point{X: int(r2Bounds.X.Hi), Y: int(r2Bounds.Y.Hi)},
+	}
+
+	// Transform r2.Points into heatmap.DataPoints
+	var data []heatmap.DataPoint
+	for _, p := range points[1:] {
+		// Invert Y since go-heatmap expects data to be ordered from bottom to top
+		data = append(data, heatmap.P(p.X, p.Y*-1))
+	}
+
+	//
+	// Drawing the image
+	//
+
+	// Load map overview image
 	fMap, err := os.Open(fmt.Sprintf("../../metadata/maps/%s.jpg", header.MapName))
 	checkError(err)
-
 	imgMap, _, err := image.Decode(fMap)
 	checkError(err)
 
-	// Create output canvas
+	// Create output canvas and use map overview image as base
 	img := image.NewRGBA(imgMap.Bounds())
-
 	draw.Draw(img, imgMap.Bounds(), imgMap, image.ZP, draw.Over)
 
-	// Generate heatmap
-	width := bounds.Max.X - bounds.Min.X
-	height := bounds.Max.Y - bounds.Min.Y
-	imgHeatmap := heatmap.Heatmap(image.Rect(0, 0, width, height), points, 15, 128, schemes.AlphaFire)
-
-	// Draw it on top of the overview
+	// Generate and draw heatmap overlay on top of the overview
+	imgHeatmap := heatmap.Heatmap(image.Rect(0, 0, bounds.Dx(), bounds.Dy()), data, dotSize, opacity, schemes.AlphaFire)
 	draw.Draw(img, bounds, imgHeatmap, image.ZP, draw.Over)
 
 	// Write to stdout
-	err = jpeg.Encode(os.Stdout, img, &jpeg.Options{
-		Quality: 90,
-	})
+	err = jpeg.Encode(os.Stdout, img, &jpeg.Options{Quality: jpegQuality})
 	checkError(err)
-}
-
-func updatedBounds(b image.Rectangle, x, y int) image.Rectangle {
-	if b.Min.X > x || b.Min.X == 0 {
-		b.Min.X = x
-	} else if b.Max.X < x {
-		b.Max.X = x
-	}
-
-	if b.Min.Y > y || b.Min.Y == 0 {
-		b.Min.Y = y
-	} else if b.Max.Y < y {
-		b.Max.Y = y
-	}
-
-	return b
 }
 
 func checkError(err error) {
