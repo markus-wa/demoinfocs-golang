ce747c3ba690a6ae34df2daa5f1a61424e9aded7
Daniel Lundgren <lundgren.daniel@gmail.com>
2019-03-21 12:58:07 +0100

    metadata: refactoring

--- metadata/maps.go
+++ metadata/maps.go
@@ -26,99 +26,27 @@ func (m Map) TranslateScale(x, y float64) (float64, float64) {
 	return x / m.Scale, y / m.Scale
 }
 
+func MakeMap(name string, x, y, scale float64) Map {
+    return Map{
+		Name: name,
+		PZero: r2.Point{
+			X: x,
+			Y: y,
+		},
+		Scale: scale,
+	}
+}
+
 // Pre-defined map translations.
-var (
-	MapDeCache = Map{
-		Name: "de_cache",
-		PZero: r2.Point{
-			X: -2000,
-			Y: 3250,
-		},
-		Scale: 5.5,
-	}
-
-	MapDeCanals = Map{
-		Name: "de_canals",
-		PZero: r2.Point{
-			X: -2496,
-			Y: 1792,
-		},
-		Scale: 4,
-	}
-
-	MapDeCbble = Map{
-		Name: "de_cbble",
-		PZero: r2.Point{
-			X: -3840,
-			Y: 3072,
-		},
-		Scale: 6,
-	}
-
-	MapDeDust2 = Map{
-		Name: "de_dust2",
-		PZero: r2.Point{
-			X: -2476,
-			Y: 3239,
-		},
-		Scale: 4.4,
-	}
-
-	MapDeInferno = Map{
-		Name: "de_inferno",
-		PZero: r2.Point{
-			X: -2087,
-			Y: 3870,
-		},
-		Scale: 4.9,
-	}
-
-	MapDeMirage = Map{
-		Name: "de_mirage",
-		PZero: r2.Point{
-			X: -3230,
-			Y: 1713,
-		},
-		Scale: 5,
-	}
-
-	MapDeNuke = Map{
-		Name: "de_nuke",
-		PZero: r2.Point{
-			X: -3453,
-			Y: 2887,
-		},
-		Scale: 7,
-	}
-
-	MapDeOverpass = Map{
-		Name: "de_overpass",
-		PZero: r2.Point{
-			X: -4831,
-			Y: 1781,
-		},
-		Scale: 5.2,
-	}
-
-	MapDeTrain = Map{
-		Name: "de_train",
-		PZero: r2.Point{
-			X: -2477,
-			Y: 2392,
-		},
-		Scale: 4.7,
-	}
-)
-
 // MapNameToMap translates a map name to a Map.
 var MapNameToMap = map[string]Map{
-	"de_cache":    MapDeCache,
-	"de_canals":   MapDeCanals,
-	"de_cbble":    MapDeCbble,
-	"de_dust2":    MapDeDust2,
-	"de_inferno":  MapDeInferno,
-	"de_mirage":   MapDeMirage,
-	"de_nuke":     MapDeNuke,
-	"de_overpass": MapDeOverpass,
-	"de_train":    MapDeTrain,
+	"de_cache":    MakeMap("de_cache",    -2000, 1250, 5.5),
+	"de_canals":   MakeMap("de_canals",   -2496, 1792, 4.0),
+	"de_cbble":    MakeMap("de_cbble",    -3840, 3072, 6.0),
+	"de_dust2":    MakeMap("de_dust2",    -2476, 3239, 4.4),
+	"de_inferno":  MakeMap("de_inferno",  -2087, 3870, 4.9),
+	"de_mirage":   MakeMap("de_mirage",   -3230, 1713, 5.0),
+	"de_nuke":     MakeMap("de_nuke",     -3453, 2887, 7.0),
+	"de_overpass": MakeMap("de_overpass", -4831, 1781, 5.2),
+	"de_train":    MakeMap("de_train",    -2477, 2392, 4.7),
 }
