df678acb0caf7beff85e88b399f3325fff9a5c7c
Arlindo Torres <arlindofontetorres@gmail.com>
2019-03-31 14:35:19 +0100

    Return Weapon IDs of Player

--- common/player.go
+++ common/player.go
@@ -105,11 +105,25 @@ func (p *Player) ActiveWeapon() *Equipment {
 	return p.RawWeapons[p.ActiveWeaponID]
 }
 
+// ActiveWeaponID returns the currently active / equipped weapon ID of the player.
+func (p *Player) ActiveWeaponID() int {
+	return int(p.RawWeapons[p.ActiveWeaponID].Weapon)
+}
+
 // Weapons returns all weapons in the player's possession.
 func (p *Player) Weapons() []*Equipment {
 	res := make([]*Equipment, 0, len(p.RawWeapons))
 	for _, w := range p.RawWeapons {
 		res = append(res, w)
+	}
+	return res
+}
+
+// WeaponIDs returns all weapon IDs in the player's possession.
+func (p *Player) WeaponIDs() []int {
+	res := make([]int, 0, len(p.RawWeapons))
+	for _, w := range p.RawWeapons {
+		res = append(res, int(w.Weapon))
 	}
 	return res
 }
