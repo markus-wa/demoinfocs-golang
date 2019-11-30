5eca39a306b6de0a780fdf5f7a5050885ff2ed17
Xavier RODET <xavier.rodet@gmail.com>
2019-11-30 12:03:11 +0100

    mopve thrownGrenades cleanup to roundOfficiallyEnded

--- game_events.go
+++ game_events.go
@@ -186,11 +186,6 @@ func newGameEventHandler(parser *Parser) gameEventHandler {
 }
 
 func (geh gameEventHandler) roundStart(data map[string]*msg.CSVCMsg_GameEventKeyT) {
-
-	// Thrown grenades could not be deleted at the end of the round (if they are thrown at the very end, they never get destroyed)
-	// So we clear them out when a new round start
-	geh.gameState().thrownGrenades = make(map[*common.Player][]*common.Equipment)
-
 	geh.dispatch(events.RoundStart{
 		TimeLimit: int(data["timelimit"].GetValLong()),
 		FragLimit: int(data["fraglimit"].GetValLong()),
@@ -242,6 +237,9 @@ func (geh gameEventHandler) roundOfficiallyEnded(data map[string]*msg.CSVCMsg_Ga
 	for _, inf := range geh.gameState().infernos {
 		geh.parser.infernoExpired(inf)
 	}
+
+	// Thrown grenades could not be deleted at the end of the round (if they are thrown at the very end, they never get destroyed)
+	geh.gameState().thrownGrenades = make(map[*common.Player][]*common.Equipment)
 
 	geh.dispatch(events.RoundEndOfficial{})
 }
