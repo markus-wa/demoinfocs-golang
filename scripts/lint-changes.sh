06fbbc58468c47f800d5de2ec40dc9b39d72fa4e
Markus Walther <mark@saiko.tech>
2024-02-09 13:32:41 +0000

    scripts: remove old travis-ci stuff

--- scripts/lint-changes.sh
+++ scripts/lint-changes.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-# for the CI
-if [[ "$TRAVIS_COMMIT_RANGE" != "" ]]; then
-	commit_range=${TRAVIS_COMMIT_RANGE/.../..} && git diff $commit_range >/dev/null && base_rev=$commit_range || true
-fi
-
 # default reference/baseline is master
 if [[ "$base_rev" == "" ]]; then
 	base_rev='origin/master'
