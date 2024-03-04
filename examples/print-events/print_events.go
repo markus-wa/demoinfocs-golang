28f108ab9d3867a3b71376581eb9032e1d017627
Markus Walther <mark@saiko.tech>
2024-03-04 10:37:59 +0000

    examples/print-events: fix scores for cs2

--- examples/print-events/print_events.go
+++ examples/print-events/print_events.go
@@ -44,9 +44,9 @@ func main() {
 		switch e.Winner {
 		case common.TeamTerrorists:
 			// Winner's score + 1 because it hasn't actually been updated yet
-			fmt.Printf("Round finished: winnerSide=T  ; score=%d:%d\n", gs.TeamTerrorists().Score()+1, gs.TeamCounterTerrorists().Score())
+			fmt.Printf("Round finished: winnerSide=T  ; score=%d:%d\n", gs.TeamTerrorists().Score(), gs.TeamCounterTerrorists().Score())
 		case common.TeamCounterTerrorists:
-			fmt.Printf("Round finished: winnerSide=CT ; score=%d:%d\n", gs.TeamCounterTerrorists().Score()+1, gs.TeamTerrorists().Score())
+			fmt.Printf("Round finished: winnerSide=CT ; score=%d:%d\n", gs.TeamCounterTerrorists().Score(), gs.TeamTerrorists().Score())
 		default:
 			// Probably match medic or something similar
 			fmt.Println("Round finished: No winner (tie)")
